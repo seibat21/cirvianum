ada5325471a6609024aea18e362e9891f251251a
Sergi Sabata <seibat21@gmail.com>
2019-09-27 16:18:50 +0200

    Canvi idioma Suma.java
    S'ha canviat l'idioma a angles

--- suma.java
+++ suma.java
@@ -1,9 +1,9 @@
 
-int numero1 = 0;
-int numero2 = 0;
+int number1 = 0;
+int number2 = 0;
 
-System.out.println("Introduce el primer numero:");
-numero1 = reader.nextInt();
+System.out.println("Write the first number:");
+number1 = reader.nextInt();
 
-System.out.println("Introduce el segundo numero:");
-numero2 = reader.nextInt();
+System.out.println("Write the second number:");
+number2 = reader.nextInt();
